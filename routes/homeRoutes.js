9dfcce081dd44c5344c5c14dcaae65448eb101df
vasyl1312 <vasylhryts@knu.ua>
2024-02-11 19:11:11 +0200

    final

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -16,6 +16,31 @@ router.post("/handleClick", (req, res) => {
   res.sendStatus(200);
 });
 
+router.post("/email", (req, res) => {
+  const { option, email } = req.body;
+
+  const msg = {
+    to: `${email}`,
+    from: `${process.env.EMAIL_FROM}`,
+    subject: "Ви обрали тур!",
+    html: ` <h1>Привіт</h1>
+    <p>Це лист з нашого марафону</p>
+    <p>Ви обрали тур: ${option}</p>
+    `,
+  };
+
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Лист було відправлено!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  res.redirect("/");
+});
+
 function saveClicksToFile(clicks) {
   fs.writeFile("clics.txt", clicks.toString(), (err) => {
     if (err) throw err;
